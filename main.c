a47a234232708b37513dd4d8acdea99d5446d123
Nguyen Tuan Dat <tuandat99kz@gmail.com>
2019-10-18 21:51:50 +0700

    Completed whole features

--- main.c
+++ main.c
@@ -8,22 +8,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define MAX_LEN_CMD 80
+#define MAX_LINE 80
 #define PROMPT "osh>"
 
-int getCmd(char *str) 
+int getCommand(char *str) 
 {
 	char ch;
 	int i = 0;
 	
-	while (((ch = getchar()) != '\n') && (i < MAX_LEN_CMD)) 
+	while (((ch = getchar()) != '\n') && (i < MAX_LINE)) 
 	{
 		str[i++] = ch;
 	}
 
-	if (i == MAX_LEN_CMD && ch != '\n')
+	if (i == MAX_LINE && ch != '\n')
 	{
-		printf("Command exceeds maximum command length\n");
+		printf("Command too long.\n");
 		i = -1;
 	}
 	else 
@@ -34,12 +34,12 @@ int getCmd(char *str)
 	return i;
 }
 
-int parseCmdToListArgs(char *cmdBuffer, char *args[])
+int parseCommand(char command[], char *args[])
 {
     int args_num = 0;
     char* arg;
 
-    while (arg = strtok_r(cmdBuffer, " ", &cmdBuffer))
+    while (arg = strtok_r(command, " ", &command))
     {
         args[args_num++] = arg;
     }
@@ -49,151 +49,253 @@ int parseCmdToListArgs(char *cmdBuffer, char *args[])
     return args_num;
 }
 
-int execCmd(char* args[], int execInBg)
+int redirectInput(char dir[])
 {
-    pid_t pid = fork();
-	if (pid < 0)
+	int fd = open(dir, O_RDONLY);
+	if (fd == -1)
 	{
-		printf("Fork process error.\n");
-		return 0;
+		printf("Can't open file.\n");
+		exit(2);
 	}
 
-	int status;
-
-	if (pid == 0)
+	if (dup2(fd, STDIN_FILENO) < 0)
 	{
-        // CHILD PROCESS
-        // printf("Child pid #%d is running\n", getpid());
-		status = execvp(args[0], args);
-		if (status == -1)
-		{
-			printf("Failed to execute the command.\n");
-		}
-		return 0;
-	} 
-	else 
-	{
-        // PARENT PROCESS
-		if (execInBg == 1)
-		{
-			printf("Pid #%d running in background.\n", pid);
-		} 
-		else 
-		{
-			pid_t childpid = wait(&status);
-			// if (WIFEXITED(status))
-			// {
-			// 	printf("Exit status: %d\n", WEXITSTATUS(status)); 
-			// }
-            // printf("Child pid #%d is terminated\n", childpid);
-		}
+		printf("Failed redirecting input.\n");
+		exit(2);
 	}
-	return 1;
+
+	close(fd);
+
+	return 0;
 }
 
-int redirectFile(char *dir, char* args[], char* buffer, int execInBg, char mode)
+int redirectOutput(char dir[])
 {
-	int fd;
-	if (mode == 'i')
-	{	
-		//open file 
-		fd = open(dir, O_RDONLY);
-		if (fd == -1)
+	int fd = creat(dir, O_WRONLY | O_CREAT | S_IRWXU);
+	if (fd == -1)
+	{
+		printf("Can't open file.\n");
+		exit(2);
+	}
+
+	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
+		printf("Failed redirecting output.\n");
+		exit(2);
+	}
+
+	close(fd);
+
+	return 0;
+}
+
+int findPipePosition(char *args[], int args_num)
+{
+	for (int i = 0; i < args_num; i++)
+	{
+		if (strcmp(args[i], "|") == 0)
 		{
-			printf("Can't open file.\n");
-			return -1;
+			return i;
 		}
-		
-		//save stdin
-		int backToStdin = dup(0);
-		close(0);
-		
-		//redirect input from keyboard to file
-		if(dup2(fd, STDIN_FILENO) == -1)
+	}
+	return -1;
+}
+
+int pipeProcesses(char *args[], int args_num, int pipePosition)
+{
+	int p[2];
+	if (pipe(p) < 0)
+	{
+		printf("Can't create pipe.\n");
+		exit(2);
+	}
+
+	// Separate args[] to 2 parts
+	char *args2[MAX_LINE];
+	for (int i = pipePosition + 1; i < args_num; i++)
+	{
+		args2[i - pipePosition - 1] = args[i];
+	}
+
+	int args2_num = args_num - pipePosition - 1;
+	args_num = pipePosition;
+
+	args[args_num] = NULL;
+	args2[args2_num] = NULL;
+
+	pid_t pidProcess2 = fork();
+
+	if (pidProcess2 < 0)
+	{
+		printf("Can't fork process.\n");
+		exit(2);
+	}
+	else if (pidProcess2 == 0)
+	{
+		if (dup2(p[0], STDIN_FILENO) < 0)
 		{
 			printf("Failed redirecting input.\n");
-			return -1;
-		}	
-		
-		//close fd (no longer need)
-		close(fd);
-		
-		//run command
-		execCmd(args, execInBg);
-		
-		//redirect input back from file to stdin
-		dup2(backToStdin, 0);
-		close(backToStdin);
-		
-		return 1;
+			exit(2);
+		}
+		close(p[0]);
+		close(p[1]);
+
+		if (execvp(args2[0], args2) == -1)
+		{
+			printf("Failed to execute the command.\n");
+			exit(2);
+		}
 	}
 	else
 	{
-		//open file 
-		fd = open(dir, O_WRONLY | O_CREAT, S_IRWXU);
-		if (fd == -1) 
+		pidProcess2 = fork();
+
+		if (pidProcess2 < 0)
 		{
-			printf("Can open file.\n");
-			return -1;
+			printf("Can't fork process.\n");
+			exit(2);
+		}
+		else if (pidProcess2 == 0)
+		{
+			if (dup2(p[1], STDOUT_FILENO) < 0)
+			{
+				printf("Failed redirecting output.\n");
+				exit(2);
+			}
+			close(p[1]);
+			close(p[0]);
+
+			if (execvp(args[0], args) == -1)
+			{
+				printf("Failed to execute the command.\n");
+				exit(2);
+			}
+		}
+		else
+		{
+			close(p[0]);
+			close(p[1]);
+			waitpid(pidProcess2, NULL, 0);
 		}
 
-		//save stdout
-		int backToStdout = dup(1);
-		close(1);
-
-		//redirect output from stdout to file
-		if(dup2(fd, STDOUT_FILENO) == -1)
-		{
-			printf("Failed redirecting output.\n");
-			return -1;
-		}
-
-		//close fd (no longer need)
-		close(fd);
-
-		//run command
-		execCmd(args, execInBg);
-
-		//redirect outout back from file to stdout
-		dup2(backToStdout, 1);
-		close(backToStdout);
-
-		return 1;
+		wait(NULL);
 	}
+}
+
+int executeCommand(char* args[], int args_num)
+{
+    // Check if redirect to input file
+	if (args_num > 2 && strcmp(args[args_num - 2], "<") == 0)
+	{
+		redirectInput(args[args_num - 1]);
+		args[args_num - 2] = NULL;
+		args_num = args_num - 2;
+	}
+
+	// Check if redirect to output file
+	if (args_num > 2 && strcmp(args[args_num - 2], ">") == 0)
+	{
+		redirectOutput(args[args_num - 1]);
+		args[args_num - 2] = NULL;
+		args_num = args_num - 2;
+	}
+
+	int pipePosition;
+
+	if (args_num > 2 && (pipePosition = findPipePosition(args, args_num)) != -1)
+	{
+		pipeProcesses(args, args_num, pipePosition);
+	}
+
+	if (execvp(args[0], args) == -1)
+	{
+		printf("Failed to execute the command.\n");
+		exit(2);
+	}
+
+	return 0;
 }
 
 int main()
 {
-    bool run = true;
+    bool running = true;
+	int flagWait;
+	char command[MAX_LINE + 1];
+	char *args[MAX_LINE / 2 + 1];
+	int args_num;
+	char preCommand[MAX_LINE + 1] = "";
+	pid_t pid;
 
-    while (run)
+    while (running)
     {
-        wait(NULL);
-        printf("%s", PROMPT);
+        flagWait = 1;
+        printf(PROMPT);
         fflush(stdout);
 
-        char cmdBuffer[MAX_LEN_CMD + 1];
+        int cmdLenght = getCommand(command);
 
-        setbuf(stdin, NULL);
-        int cmdLenght = getCmd(cmdBuffer);
+		// Can't input command or command is empty
         if (cmdLenght <= 0) continue;
 
-        char *args[MAX_LEN_CMD];
+		// Check history
+		if (strcmp(args[0], "!!") == 0)
+		{
+			if (strcmp(preCommand, "") == 0)
+			{
+				printf("No command in history.\n");
+			}
+			strcpy(command, preCommand);
+		}
 
-        int args_num = parseCmdToListArgs(cmdBuffer, args);
+		// Store current command to history
+		strcpy(preCommand, command);
 
-        if (strcmp(args[0], "exit") == 0) exit(0);
+		// Parse command to list of arguments
+		args_num = parseCommand(command, args);
 
-        int execInBg = 0;
+		// Check if exit
+        if (strcmp(args[0], "exit") == 0)
+		{
+			running = false;
+			continue;
+		}
+
+		// Check if change directory
+		if (strcmp(args[0], "cd") == 0)
+		{
+			if (chdir(args[1]) < 0)
+			{
+				printf("Can't change directory.\n");
+			}
+			continue;
+		}
+
         // Check if process executes in background
 		if (strcmp(args[args_num - 1], "&") == 0)
 		{
-			execInBg = 1;
+			flagWait = 0;
 			args[args_num - 1] = NULL;
 			args_num--;
 		}
 
-        execCmd(args, execInBg);
+		pid = fork();
+
+		if (pid == 0)	// Child process
+		{
+			executeCommand(args, args_num);
+		}
+		else if (pid > 0)	// Parent process
+		{
+			if (flagWait == 0)
+			{
+				continue;
+			}
+			waitpid(pid, NULL, 0);
+		}
+		else	// Can't fork
+		{
+			printf("Can't fork process.\n");
+		}
     }
     return 0;
 }
